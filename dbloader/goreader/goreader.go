14f1de7d6c3d0d7e87b7c58376e9746258a19162
martinmeerAT <olegmms@gmail.com3>
2025-10-06 16:38:23 +0300

    goreader(): create http-servr, add JSON parsing

--- dbloader/goreader/goreader.go
+++ dbloader/goreader/goreader.go
@@ -1,38 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"net/http"
-	"os"
+	"strings"
 )
 
 func main() {
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, playground")
+	var jsonData map[string]interface{}
+
+	http.HandleFunc("/input", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			contentType := r.Header.Get("content-type")
+			if strings.Contains(contentType, "application/json") {
+				//var jsonData map[string]interface{}
+				err := json.NewDecoder(r.Body).Decode(&jsonData)
+				if err != nil {
+					log.Println("Invalid JSON")
+					http.Error(w, "Invalid JSON", http.StatusBadRequest)
+				} else {
+					fmt.Fprintf(w, "Received JSON data: %s. Server is ready to read a file!", jsonData)
+
+				}
+			}
+		}
+
 	})
 
-	log.Println("Starting server...")
-	l, err := net.Listen("tcp", "localhost:8080")
+	log.Println("Starting server on http://localhost:8081")
+
+	listener, err := net.Listen("tcp", "localhost:8081")
 	if err != nil {
 		log.Fatal(err)
 	}
-	go func() {
-		log.Fatal(http.Serve(l, nil))
-	}()
-
-	log.Println("Sending request...")
-	res, err := http.Get("http://localhost:8080/hello")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Reading response...")
-	if _, err := io.Copy(os.Stdout, res.Body); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.Serve(listener, nil))
 
 }
